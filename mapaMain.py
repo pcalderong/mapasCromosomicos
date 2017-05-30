50dd6e9993f36b43bd21cb4dfa16de0a4ee7695c
Pcalderon <pitoc8@gmail.com>
2017-05-29 18:48:01 -0600

    Read/Write files functionality implemented

--- mapaMain.py
+++ mapaMain.py
@@ -6,6 +6,8 @@ from gi.repository import Gtk
 from gi.repository import Gdk
 from utils import getGenDescriptions
 from utils import generateMap
+from utils import saveFile
+from utils import readFileWithProb
 # from utils import MouseButtons
 
 class mapaWin:
@@ -19,6 +21,11 @@ class mapaWin:
         self.boxLabelGen = builder.get_object("boxLabelGen")
         self.boxTxtGen = builder.get_object("boxTxtGen")
         self.layoutTable = builder.get_object("fixedMatrix")
+        self.spinBtn = builder.get_object("spGens")
+        self.fileChooser = builder.get_object("fcGen")
+        self.fileChooserDialog = builder.get_object("filechooserdialogD")
+        self.fileChooserDialog.add_button("Cancel", 1)
+        self.fileChooserDialog.add_button("Ok", 2)
         self.labelGenList = []
         self.txtGenList = []
         self.arrayProb = []
@@ -27,6 +34,7 @@ class mapaWin:
         self.space = 0
         self.displayMap = False
         self.genLenght = []
+        self.countFile = 0
         window.set_default_size(1000, 1000)
         window.show_all()
         self.arrayGen = getGenDescriptions("genDescriptions.txt")
@@ -42,6 +50,7 @@ class mapaWin:
         self.drawingMap.show()
         self.boxDrawMap.pack_start(self.drawingMap, True, True, 1)
 
+    # Signal triggered to update drawing area
     def draw(self, widget, cr):
         if self.displayMap:
             cr.set_source_rgba(0, 0, 0, 1)
@@ -75,17 +84,130 @@ class mapaWin:
     #         self.drawingMap.queue_draw()
     #         print("RIGHT")
 
+    # Signal triggered by spiner when user changes the value
     def onSpinChange(self, button):
-        newQty = button.get_value_as_int()
-        if self.gens < newQty:
-            for i in range(self.gens,newQty):
+        newQty = self.spinBtn.get_value_as_int()
+        self.createTextBoxes(self.gens, newQty)
+        self.gens = self.spinBtn.get_value_as_int()
+        # self.space = 300 / self.gens
+
+    # Validation for entry values
+    def validateEmptyTextBox(self):
+        for i in range(0, self.gens):
+            if self.txtGenList[i].get_text() == '':
+                return False
+        return True
+
+    # Signal to generate the default table values
+    def onBtnGenerate(self, button):
+        self.displayMap = False
+        self.cleanTable()
+        self.arrayProb = []
+        self.generateTable(self.gens)
+
+    # Signal triggered when the entry is changed.
+    def onEntryChanged(self, entry, x, y):
+        flagRed = False
+        try:
+            value = float(entry.get_text())
+            if value <= 0.0 or value > 1.0:
+                flagRed = True
+        except:
+            entry.set_text("0.0")
+            flagRed = True
+        if flagRed:
+            entry.modify_fg(Gtk.StateFlags.NORMAL, Gdk.RGBA(1.0, 0.0, 0.0).to_color())
+        else:
+            entry.modify_fg(Gtk.StateFlags.NORMAL, Gdk.RGBA(0.0, 0.7, 0.4).to_color())
+        self.arrayProb[x][y] = entry
+        self.displayRelation()
+
+    # INFERENCE
+    def displayRelation(self):
+        i = 0
+        j = 0
+        tempFlag = True
+        self.genLenght = []
+        for row in self.arrayProb:
+            for col in row:
+                if i < j and col.get_text() == "0.0":
+                    tempFlag = False
+                elif i == 0:
+                    name = "GE" + str(j)
+                    self.maps[name] = col.get_text()
+                    self.genLenght.append(col.get_text())
+                j += 1
+            i+=1
+            j = 0
+        if not self.displayMap:
+            self.displayMap = tempFlag
+        self.drawingMap.queue_draw()
+
+    # Clean up UI on gen list side.
+    def cleanGrid(self):
+        # self.labelGenList = []
+        # self.txtGenList = []
+        self.spinBtn.set_value(0)
+
+    # Clean up UI on probability side.
+    def cleanTable(self):
+        self.genLenght = []
+        self.drawingMap.queue_draw()
+        for l in self.layoutTable:
+            self.layoutTable.remove(l)
+
+    #
+    def calculateProb(self, widget):
+        self.displayMap = generateMap(self.arrayProb)
+        self.drawingMap.queue_draw()
+
+
+    #Signal triggered when file is selected to save the filename.
+    def onFileSelected(self, widget):
+        if widget.get_filename():
+            self.fileName = widget.get_filename()
+
+    # Signal for the response dialog.
+    def onResponseDialog(self, widget, response):
+        if response == 2:
+            self.cleanTable()
+            self.cleanGrid()
+            result = readFileWithProb(self.fileName)
+            self.loadFileGen(result[0])
+            self.loadMatrix(result[1], len(result[0]))
+
+    # Reads the list of elements thar were loaded from the
+    # file and display it on the application.
+    def loadFileGen(self, list):
+        length = len(list)
+        self.arrayGen = list
+        self.spinBtn.set_value(length)
+
+    # Reads the list of elements thar were loaded from the
+    # file and display it on the application.
+    def loadMatrix(self, matrix, qty):
+        self.arrayProb = []
+        self.generateTable(qty)
+        i = 0
+        j = 0
+        for row in matrix:
+            for col in row:
+                self.arrayProb[i][j].set_text(col)
+                j+=1
+            j = 0
+            i += 1
+
+    # Generical methd to generate the UI for the gen list
+    def createTextBoxes(self, gens, newQty):
+        if gens < newQty:
+            for i in range(gens,newQty):
                 label = Gtk.Label("GE"+str(i+1))
                 self.labelGenList.append(label)
                 self.boxLabelGen.pack_start(label, True, True, 1)
                 label.show()
                 entry = Gtk.Entry()
                 strDescription = "GE"+str(i+1)
-                if len(self.arrayGen)>newQty:
+                if len(self.arrayGen)>=newQty:
                     strDescription = self.arrayGen[i-1]
                 else:
                     self.arrayGen.append(strDescription)
@@ -97,28 +219,17 @@ class mapaWin:
             for i in range(newQty, self.gens):
                 self.boxTxtGen.remove(self.txtGenList.pop())
                 self.boxLabelGen.remove(self.labelGenList.pop())
-        self.gens = button.get_value_as_int()
-        self.space = 300 / self.gens
 
-    def validateEmptyTextBox(self):
-        for i in range(0, self.gens):
-            if self.txtGenList[i].get_text() == '':
-                return False
-        return True
-
-    def onBtnGenerate(self, button):
-        self.displayMap = False
-        self.cleanTable()
-        self.arrayProb = []
+    def generateTable(self, qty):
         x = 75
         y = 0
         if self.validateEmptyTextBox():
-            for i in range(self.gens+1):
+            for i in range(qty + 1):
                 lineProb = []
-                for j in range(self.gens+1):
+                for j in range(qty + 1):
                     if i == 0:
-                        if not j == self.gens:
-                            label = Gtk.Label("GE"+str(j+1))
+                        if not j == qty:
+                            label = Gtk.Label("GE" + str(j + 1))
                             self.layoutTable.put(label, x, y)
                             label.show()
                     elif j == 0:
@@ -133,7 +244,7 @@ class mapaWin:
                             entry.modify_fg(Gtk.StateFlags.NORMAL, Gdk.RGBA(0.0, 0.0, 1.0).to_color())
                         elif i < j:
                             entry.modify_fg(Gtk.StateFlags.NORMAL, Gdk.RGBA(1.0, 0.0, 0.0).to_color())
-                            entry.connect("changed", self.onEntryChanged, i-1, j-1)
+                            entry.connect("changed", self.onEntryChanged, i - 1, j - 1)
                         else:
                             entry.modify_fg(Gtk.StateFlags.NORMAL, Gdk.RGBA(0.0, 0.0, 1.0).to_color())
                             entry.set_text("---")
@@ -141,7 +252,7 @@ class mapaWin:
                         entry.set_width_chars(5)
                         self.layoutTable.put(entry, x, y)
                         entry.show()
-                        lineProb.append(entry.get_text())
+                        lineProb.append(entry)
                     x += 75
                 y += 50
                 x = 0
@@ -150,59 +261,10 @@ class mapaWin:
         else:
             print("Make sure you fill all the gen descriptions")
 
-    def onEntryChanged(self, entry, x, y):
-        flagRed = False
-        try:
-            value = float(entry.get_text())
-            if value <= 0.0 or value > 1.0:
-                flagRed = True
-        except:
-            entry.set_text("0.0")
-            flagRed = True
-        if flagRed:
-            entry.modify_fg(Gtk.StateFlags.NORMAL, Gdk.RGBA(1.0, 0.0, 0.0).to_color())
-        else:
-            entry.modify_fg(Gtk.StateFlags.NORMAL, Gdk.RGBA(0.0, 0.7, 0.4).to_color())
-        self.arrayProb[x][y] = entry.get_text()
-        self.displayRelation()
-
-    def displayRelation(self):
-        i = 0
-        j = 0
-        tempFlag = True
-        self.genLenght = []
-        for row in self.arrayProb:
-            for col in row:
-                if i < j and col == "0.0":
-                    tempFlag = False
-                elif i == 0:
-                    name = "GE" + str(j)
-                    self.maps[name] = col
-                    self.genLenght.append(col)
-                j += 1
-            i+=1
-            j = 0
-        if not self.displayMap:
-            self.displayMap = tempFlag
-        self.drawingMap.queue_draw()
-
-    def cleanGrid(self):
-        self.labelGenList = []
-        self.txtGenList = []
-        for g in self.boxLabelGen:
-            self.boxLabelGen.remove(g)
-        for g in self.boxTxtGen:
-            self.boxTxtGen.remove(g)
-
-    def cleanTable(self):
-        self.genLenght = []
-        self.drawingMap.queue_draw()
-        for l in self.layoutTable:
-            self.layoutTable.remove(l)
-
-    def calculateProb(self, widget):
-        self.displayMap = generateMap(self.arrayProb)
-        self.drawingMap.queue_draw()
+    # Exports values inserted on the UI to save it as a file.
+    def onSaveFile(self,btn):
+        saveFile(self.txtGenList, self.arrayProb, self.countFile)
+        self.countFile+=1
 
 if __name__=="__main__":
     window = mapaWin()
--- utils.py
+++ utils.py
@@ -2,6 +2,26 @@ def getGenDescriptions(filename):
     text = readFile(filename)
     return getGens(text)
 
+def saveFile(gens, probaility, count):
+    file = open("files/recombinationFile" + str(count) + ".txt", "w")
+    file.write("Gens\n")
+    file.write("====\n")
+    i = 0
+    for g in gens:
+        file.write("GE"+str(i)+" = " + g.get_text() + "\n")
+        i += 1
+    file.write("Probability\n")
+    file.write("===========\n")
+    i = 0
+    j = 0
+    for p in probaility:
+        for col in p:
+            if i>j:
+                file.write("GE"+str(j)+" - GE"+str(i)+" = "+col.get_text()+"\n")
+            i += 1
+        j += 1
+        i =0
+    file.close()
 
 # Reads the file - Only full paths
 def readFile(fileName):
@@ -9,13 +29,49 @@ def readFile(fileName):
     text = [line.rstrip('\n') for line in open(fileName, 'r')]
     return text
 
+def readFileWithProb(fileName):
+    test = open(fileName)
+    listGens = []
+    matrixProb = []
+    text = [line.rstrip('\n') for line in open(fileName, 'r')]
+    flagGen = False
+    flagProb = False
+    for i in text:
+        if (i == "Gens"):
+            flagGen = True
+        elif (i == "Probability"):
+            flagGen = False
+            flagProb = True
+            print(listGens)
+            matrixProb = generateMatrix(len(listGens))
+        elif flagGen and not("===" in i):
+            line = i.rsplit(" = ")
+            listGens.append(line[1])
+        elif flagProb and not("===" in i):
+            line = i.rsplit(" = ")
+            index = line[0].rsplit(" - ")
+            i = int(index[0][2:])
+            j = int(index[1][2:])
+            matrixProb[i][j] = line[1]
+    return [listGens, matrixProb]
+
+def generateMatrix(size):
+    matrix = []
+    for i in range(0,size):
+        row = []
+        for j in range(0,size):
+            if i <= j:
+                row.append("0.0")
+            else:
+                row.append("---")
+        matrix.append(row)
+    return matrix
+
 def getGens(text):
     arrayGens = []
     for t in text:
         temp = getGen(t)
         arrayGens.append(temp)
-    print(arrayGens)
-
     return arrayGens
 
 def getGen(string):
